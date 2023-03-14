bd821d3f90c8039d693fb7cfe9ff9adfcf3da05c
Amir Sohail <amirsohail335@gmail.com>
2023-03-14 15:32:27 +0530

    committed

---
+++ index.js
@@ -0,0 +1,10 @@
+import { createStore } from "redux";
+import { createStore } from './node_modules/redux/src/createStore';
+
+const store = createStore(reducer);
+
+function reducer(state, action){
+    return state;
+}
+
+
