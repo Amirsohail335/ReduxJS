59c066f926fde9912ea1fd28c44621e79bd4b30c
Amir Sohail <amirsohail335@gmail.com>
2023-03-14 20:13:54 +0530

    committed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import { createStore } from "redux";
 // import { createStore } from "./node_modules/redux/src/createStore";
+// import { type } from "./node_modules/redux/index.d";
 
 const store = createStore(reducer);
 
+//reducer
 function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    return { amount: state.amount + 1 };
+  }
   return state;
 }
 
 //global state
+// console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+setInterval(() => {
+  store.dispatch({ type: "increment" });
+}, 2000);
+
+// store.dispatch({ type: "increment" });
+
 console.log(store.getState());
