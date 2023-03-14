763f445716f709cccf12f9aee6feafa73765c0bd
Amir Sohail <amirsohail335@gmail.com>
2023-03-14 15:37:25 +0530

    modified

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { createStore } from "redux";
-import { createStore } from './node_modules/redux/src/createStore';
+// import { createStore } from "./node_modules/redux/src/createStore";
 
 const store = createStore(reducer);
 
-function reducer(state, action){
-    return state;
+function reducer(state = { amount: 1 }, action) {
+  return state;
 }
 
-
+//global state
+console.log(store.getState());
